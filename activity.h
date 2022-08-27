3b2ba2943108ca5950ffc4ae6e598a8e47af57c6
a01704231 <112184077+a01704231@users.noreply.github.com>
2022-08-27 12:03:00 -0500

    Update activity.h

--- activity.h
+++ activity.h
@@ -1,47 +1,25 @@
-// =================================================================
-//
-// File: activity.h
-// Author:
-// Date:
-//
-// =================================================================
+
 #ifndef ACTIVITY_H
 #define ACTIVITY_H
 
-// =================================================================
-// sumaIterativa. Calculate the sum from 1 to n with an iterative
-// method.
-//
-// @param n	The maximum limit of the sum.
-// @return	The result of the addition from 1 to n.
-// @Complexity	??
-// =================================================================
+
 unsigned int sumaIterativa(unsigned int n) {
-	return 0;
+	int suma=0; //O(1)
+	for (int i=1;i<=n;i++){ //O(n)
+		suma+=i;
+	}
+	return suma; //O(1)
 }
 
-// =================================================================
-// sumaRecursiva. Calculate the sum from 1 to n with an recursive
-// method.
-//
-// @param n	The maximum limit of the sum.
-// @return	The result of the addition from 1 to n.
-// @Complexity	??
-// =================================================================
 unsigned int sumaRecursiva(unsigned int n) {
-	return 0;
+	if (n==0){ //O(1)
+		return 0;
+	}
+	else return n+sumaRecursiva(n-1);
 }
 
-// =================================================================
-// sumaDirecta. Calculate the sum from 1 to n with a mathematical
-// method.
-//
-// @param n	The maximum limit of the sum.
-// @return	The result of the addition from 1 to n.
-// @Complexity	??
-// =================================================================
 unsigned int sumaDirecta(unsigned int n) {
-	return 0;
+	return n*(n+1)/2;
 }
 
 #endif /* ACTIVITY_H */
