e7ac02bd27dcf7706a4bdc37b64dee5f9dc43b8e
a01704231 <112184077+a01704231@users.noreply.github.com>
2022-08-27 13:10:41 -0500

    Update activity.h

--- activity.h
+++ activity.h
@@ -3,23 +3,24 @@
 #define ACTIVITY_H
 
 
-unsigned int sumaIterativa(unsigned int n) {
-	int suma=0; //O(1)
-	for (int i=1;i<=n;i++){ //O(n)
+unsigned int sumaIterativa(unsigned int n) {      
+	int suma=0;                               //    O(1)
+	for (int i=1;i<=n;i++){                   //    O(n)
 		suma+=i;
 	}
-	return suma; //O(1)
+	return suma;                              //    O(1)
 }
 
 unsigned int sumaRecursiva(unsigned int n) {
-	if (n==0){ //O(1)
-		return 0;
+	if (n==0){                                //    O(1)
+		return 0;                         
 	}
-	else return n+sumaRecursiva(n-1);
+	else return n+sumaRecursiva(n-1);         //    O(1)
 }
 
-unsigned int sumaDirecta(unsigned int n) {
-	return n*(n+1)/2;
+unsigned int sumaDirecta(unsigned int n) {        
+	return n*(n+1)/2;                         //    O(1)
 }
 
 #endif /* ACTIVITY_H */
+az<>2222
